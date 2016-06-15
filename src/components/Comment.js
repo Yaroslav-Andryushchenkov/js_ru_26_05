d4ae4ae8f49661f396828a0849c4a1e941b2b1ed
Yaroslav Andryushchenkov <7098013@gmail.com>
2016-06-15 21:20:52 +0300

    hw 1 done Move Open/close ot Comment list component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,34 +1,18 @@
-import React, { PropTypes, Component } from 'react'
+import React, { PropTypes } from 'react'
 
-class Comment extends Component {
+function Comment(props) {
 
-    state = {
-        isOpen: false
-    }
+    const { comment } = props
 
-    render() {
-        const { comment } = this.props
-        const { isOpen } = this.state
+    if (!comment) return
 
-        if (!comment) return 
-        const body = isOpen ? <section>{comment.text}</section> : null
-        const label = isOpen ? 'Close' : 'Open'
-
-        return (
-            <div>
-                <h3>{comment.name} <a onClick = {this.toggleOpen} href='#'>{label}</a></h3>
-                {body}
-            </div>
-        )
-    }
-
-    toggleOpen = (ev) => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
+    return (
+        <div>
+            <h3>{comment.name} {comment.name} </h3>
+            <section>{comment.text}</section>
+        </div>
+    )
 }
-
 
 
 Comment.propTypes = {
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,18 +1,36 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 import Comment from './Comment'
 
 
-function CommentList(props) {
-    const { comments } = props
+class CommentList extends Component {
 
-    const commentItems = comments ? comments.map((comment) => <li key={comment.id}><Comment comment = {comment}/></li>) : null
+    state = {
+        isOpen: false
+    }
 
-    return (
-        <ul>
-            {commentItems}
-        </ul>
-    )
+    render() {
+        const { comments } = this.props
+        const commentItems = comments ? comments.map((comment) => <li key={comment.id}><Comment comment = {comment}/></li>) : null
+        const { isOpen } = this.state
+
+        if (!commentItems) return <h4>No comments</h4>
+        const label = isOpen ? 'Close' : 'Open'
+        const body = isOpen ? <ul> {commentItems} </ul> : ''
+        return (
+            <div>
+                <h4>comments: <a onClick = {this.toggleOpen} href='#'>{label}</a></h4>
+                {body}
+            </div>
+        )
+    }
+
+    toggleOpen = (ev) => {
+        this.setState({
+            isOpen: !this.state.isOpen}
+        )
+    }
 }
+
 
 CommentList.propTypes = {
     comments: PropTypes.array
