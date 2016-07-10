ae3185b359e7246c5178a3b528acec27a6b21ea2
Yaroslav Andryushchenkov <7098013@gmail.com>
2016-07-10 14:12:57 +0300

    hw4 Add comment event hanlers

--- src/AC/articles.js
+++ src/AC/articles.js
@@ -1,10 +1,20 @@
 import AppDispatcher from '../dispatcher'
-import { DELETE_ARTICLE } from '../constants'
+import { DELETE_ARTICLE, ADD_COMMENT } from '../constants'
 
 export function deleteArticle(id) {
     const action = {
         type: DELETE_ARTICLE,
         payload: { id }
+    }
+
+    AppDispatcher.dispatch(action)
+}
+
+export function addComment(comment) {
+    console.dir(comment);
+    const action = {
+        type: ADD_COMMENT,
+        payload: comment
     }
 
     AppDispatcher.dispatch(action)
--- src/components/CommentAddForm.js
+++ src/components/CommentAddForm.js
@@ -1,19 +1,41 @@
-import React, { PropTypes } from 'react'
+import React, {Component, PropTypes} from 'react'
 
-function CommentAddForm() {
-    return (
-        <div>
-            <label for="name">Name:</label>
-            <br/>
-            <input type="text" name="name" id="name" value="" size="50"></input>
-            <br/>
-            <label form="text">Text:</label>
-            <br/>
-            <textarea name="text" id="text" cols="50"></textarea>
-            <br/>
-            <input type="Button" name="add_button" value="Add"></input>
+export default class CommentAddForm extends Component {
+    state = {
+        name: "Test Name",
+        text: "Test comment"
+    }
+    
+    handleDataChange(e, prop) {
+        const buf = {};
+        buf[prop] = e.target.value;
+        this.setState(buf);
+    }
+
+    handleAddAction = (e) => {
+        alert('zzz');
+        e.preventDefault();
+        e.stopPropagation();
+        this.props.onAdd(this.state);
+    }
+    
+    render() {
+        return (
+            <div>
+                <label for="name">Name:</label>
+                <br/>
+                <input type="text" name="name" id="name" size="50" defaultValue="Test Name" onChange={e => {this.handleDataChange(e,'name')}}></input>
+                <br/>
+                <label form="text">Text:</label>
+                <br/>
+                <textarea name="text" id="text" cols="50" defaultValue="Test comment" onChange={e => {this.handleDataChange(e,'text')}}></textarea>
+                <br/>
+                <input type="Button" name="add_button" defaultValue="Add" onclick={this.handleAddAction}></input>
             </div>
-    )
+        )
+    }
 }
 
-export default CommentAddForm
+CommentAddForm.propTypes = {
+    onAdd: PropTypes.func
+}
